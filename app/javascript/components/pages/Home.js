58413b2e30595bf23358e204712a1c5cbcd3c930
sjproctor <sjproctor950@gmail.com>
2021-08-30 17:22:48 -0700

    adds home page image

---
+++ app/javascript/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import coloredDoors from '../assets/many-colorful-doors.jpeg'
+
+class Home extends Component {
+  render() {
+    return (
+      <>
+        <img src={coloredDoors} alt="series of colorful doors" className="colored-doors"/>
+      </>
+    )
+  }
+}
+export default Home
