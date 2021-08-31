0c15a351214ac5126e640260283833a3610b164c
sjproctor <sjproctor950@gmail.com>
2021-08-31 13:45:01 -0700

    adds testing for index and mocks for images

--- app/javascript/components/components/Header.test.js
+++ app/javascript/components/components/Header.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import Header from './Header'
+import mockPic from '../fileMock.js'
 
 Enzyme.configure({adapter: new Adapter()})
 
@@ -10,5 +11,9 @@ describe("When Header renders", () => {
     const header = shallow(<Header />)
     const headerNav = header.find("ul")
     expect(headerNav.length).toEqual(2)
+  })
+  it("displays an image", () => {
+    const logo = shallow(<Header />)
+    expect(logo.find("img").prop("src")).toEqual(mockPic);
   })
 })
